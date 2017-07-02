8b679cb614ccfae26f377712cb5793dc0cac7985
evandrordacunha <evandrordacunha@gmail.com>
2017-07-02 19:32:04 -0300

    v 5.0

--- LeilaoTF/src/dados/CadastroPessoaDAOJavaDb.java
+++ LeilaoTF/src/dados/CadastroPessoaDAOJavaDb.java
@@ -2,8 +2,11 @@ package dados;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import negocio.Pessoa;
 
 public class CadastroPessoaDAOJavaDb {
 	
@@ -35,11 +38,12 @@ private static CadastroPessoaDAOJavaDb ref;
         try {
             Connection con = DriverManager.getConnection("jdbc:derby:derbyDB;create=true");
             Statement sta = con.createStatement();
-            String sql = "CREATE TABLE Pessoas ("
+            String sql = 
+            		"CREATE TABLE Pessoas ("
                     + "ID INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
-                    + "NOME VARCHAR(100) NOT NULL,"
-                    + "TELEFONE CHAR(8) NOT NULL,"
-                    + "SEXO CHAR(1) NOT NULL"
+                    + "EMAIL VARCHAR(50),"
+                    + "LOGIN VARCHAR(50),"
+                    + "SENHA VARCHAR(50),"
                     + ")";
             sta.executeUpdate(sql);
             sta.close();
@@ -49,5 +53,32 @@ private static CadastroPessoaDAOJavaDb ref;
         }
     }
     
+    private static Connection getConnection() throws SQLException {
+        //derbyDB sera o nome do diretorio criado localmente
+        return DriverManager.getConnection("jdbc:derby:derbyDB");
+    }
+    /*
+    @Override
+    public boolean adicionar(Pessoa p) throws CadastroDAOException {
+        try {
+            Connection con = getConnection();
+            PreparedStatement stmt = con.prepareStatement(
+                    "INSERT INTO PESSOAS (NOME, TELEFONE, SEXO) VALUES (?,?,?)" //                             1        2         3            4          5             6
+                    );
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getTelefone());
+            stmt.setString(3, Character.toString(p.getSexo()));
+            int ret = stmt.executeUpdate();
+            con.close();
+            return (ret>0);
+        } catch (SQLException ex) {
+            throw new CadastroDAOException("Falha ao adicionar.", ex);
+        }
+    }
+    */
 
+    public static void main(String[] args) throws CadastroDAOException {
+		CadastroPessoaDAOJavaDb c = new  CadastroPessoaDAOJavaDb();
+	
+	}
 }
